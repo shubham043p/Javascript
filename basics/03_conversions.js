9e49703d01799381fe7d19b8b46b6327e904b91b
Shubham Panwar <144083909+shubham043p@users.noreply.github.com>
2024-01-29 17:40:13 +0000

    conversion of datta types

---
+++ basics/03_conversions.js
@@ -0,0 +1,15 @@
+// let score  = "33";
+// // doing conversion
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1;
+// let booleanIsloggedin = Boolean(isLoggedIn);
+// console.log(booleanIsloggedin);
+
+let number = 33;
+let string = String(number);
+console.log(string);
+console.log(typeof string);
