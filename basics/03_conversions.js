f189cbf78676e4b9fec9be159def7a3f55ba1a54
Shubham Panwar <144083909+shubham043p@users.noreply.github.com>
2024-01-29 17:55:16 +0000

    updated

--- basics/03_conversions.js
+++ basics/03_conversions.js
@@ -9,7 +9,20 @@
 // let booleanIsloggedin = Boolean(isLoggedIn);
 // console.log(booleanIsloggedin);
 
-let number = 33;
-let string = String(number);
-console.log(string);
-console.log(typeof string);
+// let number = 33;
+// let string = String(number);
+// console.log(string);
+// console.log(typeof string);
+
+// *******operations********
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%
+
+let s1 = "shubham"
+let s2 = "panwar"
+let s = s1+s2;
+console.log(s);
