9c0ea3ad2dec392fbbed8e4c644f8c13be6f8e1b
Shubham Panwar <144083909+shubham043p@users.noreply.github.com>
2024-01-29 17:24:40 +0000

    data types

---
+++ basics/02_DataTypes.js
@@ -0,0 +1,13 @@
+"use strict" // sara code newer version ke hisab se treat kiya jayega
+// alert( ) ko use krne ke liye alag syntax hai node me
+console.log(3+3);
+console.log("shubham")
+let age = 29
+let name = "shubham"
+let isOccupied = true; // 
+// type of will return type of data
+
+console.log(typeof name);
+console.log(typeof isOccupied);
+console.log(typeof null);
+console.log(typeof undefined);
