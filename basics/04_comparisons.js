7d62ba63b813d8534d39f4865881ca63e3d9771a
Shubham Panwar <144083909+shubham043p@users.noreply.github.com>
2024-02-26 13:01:12 +0000

    update

--- basics/04_comparisons.js
+++ basics/04_comparisons.js
@@ -1,5 +1,9 @@
-console.log(1==2);
-console.log(1!=2)
-
-// === triple equal is used for strict check 
-console.log("2"===2);
+// console.log(2 >= 1)
+// console.log("2">=1)
+// console.log(null >= 0)
+// console.log(null<=0)
+// console.log(null==0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+// console.log(undefined<=0)
+console.log("2"===2)
---
+++ basics/stringsOperation.js
@@ -0,0 +1,7 @@
+// let str1 = "shubham"
+// let str2 = " panwar"
+// let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1" + 3 + 3)
+console.log(1+3+'5')
