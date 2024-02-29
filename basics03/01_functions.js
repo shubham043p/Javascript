32a18566caa70082849459ce7459bd5bf8d15f84
Shubham Panwar <144083909+shubham043p@users.noreply.github.com>
2024-02-29 01:27:42 +0000

    update

--- basics03/01_functions.js
+++ basics03/01_functions.js
@@ -1,21 +1,41 @@
-// function saymyname(){
-//     console.log("s");
-//     console.log("p");
-// }
-// saymyname();
+// // function saymyname(){
+// //     console.log("s");
+// //     console.log("p");
+// // }
+// // saymyname();
+// // function addtwonumbers(n1,n2){
+// //     console.log(n1+n2);
+// // }
+// // addtwonumbers(1,"2");
+
 // function addtwonumbers(n1,n2){
-//     console.log(n1+n2);
+//     return n1+n2;
 // }
-// addtwonumbers(1,"2");
+// const result = addtwonumbers(1,2);
+// console.log(`Result is : ${result}`)
 
-function addtwonumbers(n1,n2){
-    return n1+n2;
-}
-const result = addtwonumbers(1,2);
-console.log(`Result is : ${result}`)
+// function loggedIN(username = "rishabh"){
+//     return `${username} just logged in!!`
+// }
+// // console.log(loggedIN())
+// console.log(loggedIN("shubham"))
 
-function loggedIN(username = "rishabh"){
-    return `${username} just logged in!!`
+// function calculateCartPrice(...num1){
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(100,200,200,300));
+
+// function r(val1,val2,...num1){
+//     return num1;
+// }
+// console.log(r(1,2,2,3,3,4,45))
+
+const user = {
+    username: "shubham",
+    age:"19"
 }
-// console.log(loggedIN())
-console.log(loggedIN("shubham"))
+
+function UserDetail(user){
+    console.log(`username is ${user.username} and age is ${user}`)
+}
