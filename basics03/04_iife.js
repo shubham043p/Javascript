437c6ac6a484ab7246c0868a15c77910b886f78a
Shubham Panwar <144083909+shubham043p@users.noreply.github.com>
2024-03-01 01:16:57 +0000

    changes

---
+++ basics03/04_iife.js
@@ -0,0 +1,12 @@
+(function chai(){
+    console.log("DB connnected")
+})();
+
+(()=>{
+    console.log('db connected')
+})();
+
+// const f = ()=> console.log('hello');
+// f();
+((name)=>{console.log(`${name} , hi bebiiiii`)}
+)("shubham");
