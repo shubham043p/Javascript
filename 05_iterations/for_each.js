9f51168d2e91e31bb440b7ab944b5f7fabfd244d
Shubham Panwar <144083909+shubham043p@users.noreply.github.com>
2024-03-02 02:27:45 +0000

    changes

--- 05_iterations/for_each.js
+++ 05_iterations/for_each.js
@@ -1,5 +1,34 @@
 const languages = ["cpp","java",'c','python']
 
-languages.forEach(function(val){
-    console.log(val);
-})
+// languages.forEach(function(val){
+//     console.log(val);
+// })
+
+// function print(val){
+//     console.log(val);
+// }
+
+// languages.forEach(print) // yahan ab print reference ke through pass krna hai na ki execute kr den hai print() matlb execute and print only is reference to the function
+
+
+// languages.forEach((item,index,arr) => {
+//     console.log(item,index,arr);
+// });
+
+const myObject = [
+    {
+        filename: "java",
+        file :"j"
+    },
+    {
+        filename: "c",
+        file :"c"
+    },
+    {
+        filename: "c",
+        file :"c"
+    }
+]
+myObject.forEach((element) => {
+    console.log(element.file);
+});
